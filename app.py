ba9007b667cc09236b4938f2b64346bd4fb5ce71
Shamanth vallem <srvallem@gmail.com>
2023-05-30 02:13:29 +0530

    adding to github

---
+++ app.py
@@ -0,0 +1,100 @@
+from flask import Flask, render_template, request
+import numpy as np
+from simplex import simplexMethod, optimalValues
+from revised_simplex import revisedSimplexMethod
+app = Flask(__name__) 
+
+@app.route("/") 
+def data():
+    return render_template("data.html")
+
+
+@app.route("/calculate", methods = ['post'])
+def calculate():
+    global minOrMax
+    minOrMax = (request.form["minOrMax"])
+    # global function
+    # function = (request.form["function"])
+    global numOfConstraints
+    numOfConstraints = int(request.form['numOfConstraints'])
+    global inequalityConstraints
+    inequalityConstraints = int(request.form['numOfInequalityConstraints'])
+    coeff_value_list = [6, 4, 7, 5]
+    ineq_value_list = [[1, 2, 1, 2, 20], [6, 5, 3, 2, 100], [3, 4, 9, 12, 75]]
+    return render_template("calculate.html", minOrMax = minOrMax, numOfConstraints = numOfConstraints, inequalityConstraints = inequalityConstraints, coeff_value_list=coeff_value_list, ineq_value_list=ineq_value_list)
+    # return render_template("calculate.html", minOrMax = minOrMax, numOfConstraints = numOfConstraints, inequalityConstraints = inequalityConstraints)
+
+
+@app.route("/output", methods = ["post"])
+def output():
+    
+    global functionCoeffecientMatrix
+    # functionCoeffecientMatrix = np.zeros((1, int(numOfConstraints - inequalityConstraints)))
+    functionCoeffecientMatrix = []
+    for i in range(int(numOfConstraints - inequalityConstraints)):
+        # functionCoeffecientMatrix[i] = request.form["objcof"+str(i)]
+        functionCoeffecientMatrix.append(int(request.form["objcof"+str(i)]))
+
+
+    global inequalityMatrix
+    inequalityMatrix = np.zeros((inequalityConstraints, int(numOfConstraints - inequalityConstraints)))
+    for i in range(inequalityConstraints):
+        for j in range(int(numOfConstraints - inequalityConstraints)):
+            inequalityMatrix[i][j] = request.form["iqrsts"+str(i)+str(j)]
+
+    global constantMatrix
+    # constantMatrix = np.zeros((1, inequalityConstraints))
+    constantMatrix = []
+    for i in range(inequalityConstraints):
+        # constantMatrix[i] = request.form["iqrsts"+str(i)+str(int(numOfConstraints - inequalityConstraints))]
+        constantMatrix.append(int(request.form["iqrsts"+str(i)+str(int(numOfConstraints - inequalityConstraints))]))
+
+    global method
+    method = request.form["methods"]
+
+    global finalResult
+    if(method == "simplex"):
+        finalResult = simplexMethod(functionCoeffecientMatrix, inequalityMatrix, constantMatrix, minOrMax)
+        optimalValues_list = optimalValues(finalResult[-1])
+    elif(method == "revisedSimplex"):
+        finalResult = revisedSimplexMethod(functionCoeffecientMatrix, inequalityMatrix, constantMatrix, minOrMax)
+
+
+    return render_template("output.html", minOrMax = minOrMax, numOfConstraints = numOfConstraints, inequalityConstraints = inequalityConstraints, functionCoeffecientMatrix = functionCoeffecientMatrix, inequalityMatrix = inequalityMatrix, constantMatrix = constantMatrix, method = method, finalResult = finalResult,optimalValues_list = optimalValues_list)
+
+# Function to extract coefficients from given equation
+def extractVariables(eq):
+    eq = eq.split()
+    removez = []
+    equaltity = False
+
+    for i in range(len(eq)):
+        if(i%2 == 1):
+            if(eq[i] == "-"):
+                eq[i+1] = "-"+eq[i+1]
+            if(eq[i] == "<" or eq[i] == ">" or eq[i] == "="):
+                eq[i+1] = eq[i+1]+"constant"
+                equaltity = True
+            removez.append(eq[i])
+
+    for i in eq:
+        if(i in removez):
+            eq.remove(i)
+
+    coeff = {}
+    for i in range(len(eq)):
+        if(i == len(eq)-1 and equaltity == True):
+            var = "constant"
+            num = eq[i][:-8]
+        else:
+            var = eq[i][-1]
+            num = eq[i][:-1]
+        if(num == ""):
+            num = 1
+        elif(num == "-"):
+            num = -1
+        coeff[var] = num
+    return coeff
+
+if __name__ == "__main__":
+    app.run(debug = True)
---
+++ simplex.py
@@ -0,0 +1,117 @@
+import numpy as np
+
+def simplexMethod(functionCoeffecientMatrix, inequalityMatrix, constantMatrix, minOrMax):
+    m, n = inequalityMatrix.shape
+#     print(m)
+#     print(n)
+    tobeReturned = []
+    bfs_list = []
+    pivotValues_list = []
+    np.set_printoptions(suppress=True)
+    tableau = np.zeros((m + 1, n + m + 1))
+
+    # final_bfs = []
+#     print(tableau)
+
+#   filling tableau with values of A
+    for i in range(m):
+        for j in range(n):
+            tableau[i][j] = inequalityMatrix[i][j]
+#   filling tableau with values of b
+    for i in range(m):
+        tableau[i][-1] = constantMatrix[i]
+#   fill tableau with values of c
+    if(minOrMax == "minimization"):
+        for i in range(len(functionCoeffecientMatrix)):
+            tableau[-1][i] = functionCoeffecientMatrix[i]
+    else:
+        for i in range(len(functionCoeffecientMatrix)):
+            tableau[-1][i] = (-1)*functionCoeffecientMatrix[i]
+
+#     print(tableau)
+    v, d = tableau.shape
+
+    # Adding slack variables
+    for i in range(m):
+        for j in range(n, d-2):
+            tableau[i][j+i] = 1
+            break
+    initial_tableau = tableau.copy()
+    tobeReturned.append(initial_tableau)
+            
+    # print(tableau)b
+#     i = 0
+    while np.any(tableau[-1, :-1] < 0):
+        pivot_column = np.argmin(tableau[-1, :-1])
+        ratios = tableau[:-1, -1] / tableau[:-1, pivot_column]
+        pivot_row = np.argmin(ratios[ratios >= 0])
+        pivot_value = tableau[pivot_row, pivot_column]
+
+        pivotValues_list.append([pivot_value, pivot_row, pivot_column])
+#         if(i == 0):
+#             print(pivot_row)
+#             print(pivot_value)
+#         i = i+1
+        # bfs_list.append(tableau)
+        bfs_list.append(tableau.copy())
+
+        # print_tableau(tableau)  # Print the current tableau 
+#         Pivot operation
+        tableau[pivot_row, :] /= pivot_value
+        for i in range(m + 1):
+            if i != pivot_row:
+                tableau[i, :] -= tableau[i, pivot_column] * tableau[pivot_row, :]
+    # final_bfs.append(tableau)
+    final_bfs = tableau.copy()
+    tobeReturned.append(bfs_list)
+    tobeReturned.append(pivotValues_list)
+    tobeReturned.append(final_bfs)
+    # print("Final tableau")
+    # print_tableau(tableau)  # Print the final tableau
+
+    # tobeReturned.append(bfs_list)
+    # tobeReturned.append(final_bfs)
+    # return tableau[-1, -1]
+    return tobeReturned
+
+# def print_tableau(tableau):
+#     m, n = tableau.shape
+#     for i in range(m):
+#         for j in range(n):
+#             print("{:.2f}".format(tableau[i, j]), end="\t")
+#         print()
+#     print("---------------------")
+
+def optimalValues(optimizedTableau):
+    count = 0
+    flag = 0
+    res_list = []
+    for i in range(len(optimizedTableau[0])-1):
+        count = 0
+        flag = 0
+        for j in range(len(optimizedTableau)-1):
+            # print(j)
+            # print(i)
+            if(optimizedTableau[j][i] != 0 and optimizedTableau[j][i] != 1):
+                # print(optimizedTableau[j][i])
+                # print("Im here 1")
+                count = 0
+                flag = 0
+                # break
+            elif(optimizedTableau[j][i] == 1 and count > 0):
+                # print("Im here 2")
+                count = 0
+                flag = 0
+                # break
+            elif(optimizedTableau[j][i] == 1):
+                # print("Im here 3")
+                flag = 1
+                temp_var_i = i
+                temp_var_j = j
+                count = count + 1
+        if(flag == 1):
+            res_list.append([temp_var_i, temp_var_j])
+            print(res_list)
+    return res_list
+
+                
